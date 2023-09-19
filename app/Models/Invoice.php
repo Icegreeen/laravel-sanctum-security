03f18a853de56703ebc0bddc7afbb92753717447
Icegreeen <flavioaquila16@gmail.com>
2023-09-19 06:21:27 -0300

    designing database

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -5,12 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Customer extends Model
+class Invoice extends Model
 {
     use HasFactory;
 
-    public function invoices()
+    public function customer()
     {
-        return $this->hasMany(Invoice::class);
+        return $this->belongsTo(Customer::class);
     }
 }
--- database/factories/CustomerFactory.php
+++ database/factories/CustomerFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -16,8 +17,18 @@ class CustomerFactory extends Factory
      */
     public function definition(): array
     {
+
+        $type = $this->faker->randomElement(['I', 'B']);
+        $name = $type == 'I' ? $this->faker->name() : $this->faker->company();
+
         return [
-            //
+            'name' => $name,
+            'type' => $type,
+            'email' => $this->faker->email(),
+            'address' => $this->faker->streetAddress(),
+            'city' => $this->faker->city(),
+            'state' => $this->faker->state(),
+            'postal_code' => $this->faker->postCode(),
         ];
     }
 }
